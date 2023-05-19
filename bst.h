ea43f9ebdae78d15ca7150c671546a5ced47d675
FlavioLux <42012471+FlavioLux@users.noreply.github.com>
2023-05-18 23:24:11 -0300

    Add files via upload
    Implementação de Árvore Binária de Busca em C

---
+++ bst.h
@@ -0,0 +1,195 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/queue.h>
+#include <unistd.h>
+
+const int INT_MIN = -2147483647;
+
+/*************************************
+ * Fila para navegacao por niveis
+ *************************************/
+TAILQ_HEAD(tailhead, entry) head;
+
+struct entry {
+  struct noBst *raiz;
+  TAILQ_ENTRY(entry) entries;
+};
+
+void add_to_queue(struct noBst *raiz) {
+  struct entry *elem = (struct entry *)malloc(sizeof(struct entry));
+  if (elem)
+    elem->raiz = raiz;
+  TAILQ_INSERT_TAIL(&head, elem, entries);
+}
+/*************************************/
+
+struct noBst {
+  struct noBst *esq;
+  int val;
+  struct noBst *dir;
+};
+
+void imprimir(struct noBst *raiz) {
+  if (raiz != NULL) {
+    imprimir(raiz->esq);
+    printf("%d, ", raiz->val);
+    imprimir(raiz->dir);
+  }
+}
+
+/**
+ * Funcao que aloca um novo noBst.
+ **/
+struct noBst *alocarNo(int val) {
+  struct noBst *no = (struct noBst *)malloc(sizeof(struct noBst));
+  no->esq = NULL;
+  no->val = val;
+  no->dir = NULL;
+  return no;
+}
+
+/**
+ * Funcao recursiva para inserir novo no.
+ **/
+void inserir(struct noBst **raiz, int val) {
+  if ((*raiz) == NULL)
+    (*raiz) = alocarNo(val);
+  else {
+    if (val > (*raiz)->val)
+      inserir(&(*raiz)->dir, val);
+    else
+      inserir(&(*raiz)->esq, val);
+  }
+}
+
+/**
+ * Funcao recursiva para buscar valor.
+ **/
+bool buscar(struct noBst *raiz, int val) {
+  if (raiz == NULL) {
+    return false;
+  } else {
+    if (raiz->val == val)
+      return true;
+    else if (raiz->val > val)
+      return buscar(raiz->esq, val);
+    else
+      return buscar(raiz->dir, val);
+  }
+}
+
+int minIt(struct noBst *raiz) { // raiz eh local
+  if (raiz == NULL)
+    return INT_MIN;
+  while (raiz->esq != NULL)
+    raiz = raiz->esq;
+  return raiz->val;
+}
+
+int minRec(struct noBst *raiz) { // raiz eh local
+  if (raiz == NULL)
+    return INT_MIN;
+  else if (raiz->esq != NULL)
+    return minRec(raiz->esq);
+  return raiz->val;
+}
+
+int maxBST(struct noBst *raiz) { // raiz eh local
+  if (raiz == NULL)
+    return INT_MIN;
+  else if (raiz->dir != NULL)
+    return maxBST(raiz->dir);
+  return raiz->val;
+}
+
+int max(int a, int b) {
+  if (a > b)
+    return a;
+  else
+    return b;
+}
+
+int altura(struct noBst *raiz) {
+  if (raiz == NULL)
+    return -1;
+  else {
+    return 1 + max(altura(raiz->esq), altura(raiz->dir));
+  }
+}
+
+void navNivel(struct noBst *raiz) {
+  TAILQ_INIT(&head);
+  // add a raiz na fila
+  add_to_queue(raiz);
+
+  struct entry *elem = head.tqh_first;
+  // enquanto a fila não estiver vazia
+  while (elem != NULL) {
+    struct noBst *noVisitado = elem->raiz;
+    // imprimir/processar o elemento na frente da fila
+    printf("%d, ", noVisitado->val);
+    // adicionar as sub-árvores à fila
+    if (noVisitado->esq != NULL)
+      add_to_queue(noVisitado->esq);
+    if (noVisitado->dir != NULL)
+      add_to_queue(noVisitado->dir);
+
+    // remover o nó que está na frente da fila
+    TAILQ_REMOVE(&head, elem, entries);
+    free(elem);
+    elem = head.tqh_first;
+  }
+}
+
+void navPreOrdem(struct noBst *raiz) {
+  if (raiz != NULL) {
+    printf("%d, ", raiz->val);
+    navPreOrdem(raiz->esq);
+    navPreOrdem(raiz->dir);
+  }
+}
+
+void navEmOrdem(struct noBst *raiz) {
+  if (raiz != NULL) {
+    navEmOrdem(raiz->esq);
+    printf("%d, ", raiz->val);
+    navEmOrdem(raiz->dir);
+  }
+}
+
+void navPosOrdem(struct noBst *raiz) {
+  if (raiz != NULL) {
+    navPosOrdem(raiz->esq);
+    navPosOrdem(raiz->dir);
+    printf("%d, ", raiz->val);
+  }
+}
+
+struct noBst *remover(struct noBst *raiz, int val) {
+  if (raiz == NULL)
+    return raiz;
+  else if (val < raiz->val)
+    raiz->esq = remover(raiz->esq, val);
+  else if (val > raiz->val)
+    raiz->dir = remover(raiz->dir, val);
+  else { // val == raiz->val
+    if (raiz->esq == NULL && raiz->dir == NULL) {
+      free(raiz);
+      raiz = NULL;
+    } else if (raiz->dir == NULL) {
+      struct noBst *noASerRemovido = raiz;
+      raiz = raiz->esq;
+      free(noASerRemovido);
+    } else if (raiz->esq == NULL) {
+      struct noBst *noASerRemovido = raiz;
+      raiz = raiz->dir;
+      free(noASerRemovido);
+    } else { // possui dois filhos
+      int maxEsq = maxBST(raiz->esq);
+      raiz->val = maxEsq;
+      raiz->esq = remover(raiz->esq, maxEsq);
+    }
+  }
+  return raiz;
+}
